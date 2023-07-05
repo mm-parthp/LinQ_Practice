e8be0f14a1960bb566a15168ae910a432b5bf940
mm-parthp <mm.parthpanchal@gmail.com>
2023-07-05 16:24:24 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ_Practice
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+
+        public string EmployeeDes { get; set; }
+        public int ProjectId { get; set; }
+
+    }
+}
